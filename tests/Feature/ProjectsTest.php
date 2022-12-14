da0304cca643910e67d55db0f0cafd9213323bd7
adnrby <adrian@nrby.com>
2022-12-14 19:52:49 +0800

    Database assertion on test

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -22,6 +22,8 @@ class ProjectsTest extends TestCase
 
         $this->post('/projects', $attributes)->assertRedirect('/projects');
 
+        $this->assertDatabaseHas('projects', $attributes);
+
         $this->get('/projects')->assertSee($attributes['title']);
     }
 }
