27d9b947590f05a611502c18e3205d8cb9a2d301
adnrby <adrian@nrby.com>
2022-12-26 21:53:10 +0800

    Redesigining the project index page

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,24 +1,30 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Projects') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <ul>
-                        @forelse($projects as $project)
-                            <li>
-                                <a href="{{ $project->path() }}">{{ $project->title }}</a>
-                            </li>
-                        @empty
-                            <li>No Project Created</li>
-                        @endforelse
-                    </ul>
-                </div>
+            <div class=overflow-hidden">
+                <ul class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                    @forelse($projects as $project)
+                        <li class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
+                            <div class="flex w-full items-center justify-between space-x-6 p-6">
+                                <div class="flex-1 truncate">
+                                    <div class="flex items-center space-x-3">
+                                        <h3 class="truncate text-m font-medium text-gray-900 font-bold">{{ $project->title }}</h3>
+                                    </div>
+                                    <p class="mt-1 text-gray-500">{{ $project->description }}</p>
+                                </div>
+                            </div>
+                            <div>
+
+                            </div>
+                        </li>
+                    @empty
+                        <li>
+                            <div>
+                                No Project Created
+                            </div>
+                        </li>
+                    @endforelse
+                </ul>
             </div>
         </div>
     </div>
