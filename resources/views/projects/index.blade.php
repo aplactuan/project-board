b69b90c218a97cabaa41390e55025742687c99b6
adnrby <adrian@nrby.com>
2023-01-02 21:19:10 +0800

    Partial design for the project detail page

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -9,11 +9,13 @@
                 <div>
                     <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
                         @forelse($projects as $project)
-                            <div class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
+                            <div class="card">
                                 <div class="flex w-full items-center justify-between space-x-6 p-6">
                                     <div class="flex-1 truncate">
                                         <div class="flex items-center space-x-3">
-                                            <h3 class="truncate text-m font-medium text-gray-900 font-bold">{{ $project->title }}</h3>
+                                            <h3 class="truncate text-m font-medium text-gray-900 font-bold">
+                                                <a href="{{ $project->path() }}">{{ $project->title }}</a>
+                                            </h3>
                                         </div>
                                         <p class="mt-1 text-gray-500">{{ $project->description }}</p>
                                     </div>
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,17 +1,31 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ $project->title }}
-        </h2>
+
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ $project->description }}
+    <main>
+        <div class="lg:flex max-w-7xl mx-auto sm:px-6 lg:px-8 py-12">
+            <div class="mb-4 lg:w-3/4 space-y-4">
+                <h3 class="font-lg mb-3 text-gray-500">Tasks</h3>
+                <div class="space-y-2">
+                    <div class="card p-4">
+                        Task 1
+                    </div>
+                    <div class="card p-4">
+                        Task 2
+                    </div>
+                    <div class="card p-4">
+                        Task 3
+                    </div>
+                </div>
+                <h3 class="font-lg mb-3 text-gray-500">General Notes</h3>
+                <div class="card">General Notes</div>
+            </div>
+            <div class="lg:w-1/4 px-2">
+                <div class="card p-4">
+                    {{ $project->title }}
                 </div>
             </div>
         </div>
-    </div>
+    </main>
 </x-app-layout>
