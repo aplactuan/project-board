b1b3378cdab92e1d39f8e00e8c2636830447d922
adnrby <adrian@nrby.com>
2023-01-26 03:26:23 +0800

    Product index page changes

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,39 +1,43 @@
 <x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class=overflow-hidden">
-                <div class="flex items-center justify-between my-5">
-                    <h2 class="text-gray-500 text-lg">My Projects</h2>
-                    <a href="/projects/create" class="btn btn-blue">Add Projects</a>
-                </div>
-                <div>
-                    <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-                        @forelse($projects as $project)
-                            <div class="card">
-                                <div class="flex w-full items-center justify-between space-x-6 p-6">
-                                    <div class="flex-1 truncate">
-                                        <div class="flex items-center space-x-3">
-                                            <h3 class="truncate text-m font-medium text-gray-900 font-bold">
-                                                <a href="{{ $project->path() }}">{{ $project->title }}</a>
-                                            </h3>
-                                        </div>
-                                        <p class="mt-1 text-gray-500">{{ $project->description }}</p>
-                                    </div>
-                                </div>
-                                <div>
-
-                                </div>
-                            </div>
-                        @empty
-                            <li>
-                                <div>
-                                    No Project Created
-                                </div>
-                            </li>
-                        @endforelse
-                    </div>
-                </div>
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <div class="flex items-center mb-3">
+            <a href="/projects/create">Create Project</a>
+        </div>
+        <div class="flex">
+            <div class="w-1/4">
+                <h3>Title</h3>
+                <div>Lorem Ipsum</div>
             </div>
+            <div class="w-1/4">
+                <h3>Title</h3>
+                <div>Lorem Ipsum</div>
+            </div>
+            <div class="w-1/4">
+                <h3>Title</h3>
+                <div>Lorem Ipsum</div>
+            </div>
+            <div class="w-1/4">
+                <h3>Title</h3>
+                <div>Lorem Ipsum</div>
+            </div>
+            <div class="w-1/4">
+                <h3>Title</h3>
+                <div>Lorem Ipsum</div>
+            </div>
+            <div class="w-1/4">
+                <h3>Title</h3>
+                <div>Lorem Ipsum</div>
+            </div>
+        </div>
+        <div class="flex">
+            @forelse ($projects as $project)
+                <div class="w-1/4 bg-white rounded shadow">
+                    <h3>{{ $project->title }}</h3>
+                    <div>{{ $project->description }}</div>
+                </div>
+            @empty
+                <div>No projects yet</div>
+            @endforelse
         </div>
     </div>
 </x-app-layout>
