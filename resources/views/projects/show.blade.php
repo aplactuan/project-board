c9d95a87a125adf82b5706ee8bb545bdefcf7a08
aplactuan <aplactuan@gmail.com>
2023-02-09 20:10:57 +0800

    Update on the general notes section

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,7 +2,9 @@
     <main>
         <div class="max-w-7xl mx-auto lg:px-8 py-12 sm:px-6">
             <div class="lg:flex items-center mb-3 justify-between py-4">
-                <h2 class="font-normal text-lg">{{ $project->title }}</h2>
+                <p class="font-normal text-lg">
+                    <a href="/projects">My Projects</a> / {{ $project->title }}
+                </p>
                 <a href="{{ $project->path() . '/edit' }}" class="btn btn-blue">Edit Project</a>
             </div>
             <div class="lg:flex">
@@ -30,10 +32,8 @@
                             </div>
                     </div>
                     <h3 class="font-lg mb-3 text-gray-500">General Notes</h3>
-                    <div class="card">{{ $project->notes }}</div>
-                    <div>
-                        <a href="/projects">Go back</a>
-                    </div>
+                    <textarea class="card w-full" style="min-height: 200px">{{ $project->notes }}</textarea>
+
                 </div>
                 <div class="lg:w-1/4 px-2">
                     <div class="card p-4">
@@ -41,6 +41,9 @@
                         {{ $project->description }}
                     </div>
                 </div>
+            </div>
+            <div>
+                <a href="/projects" class="btn btn-blue">Go back</a>
             </div>
         </div>
 
