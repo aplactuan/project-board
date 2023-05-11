5a327f9977ef28f59a2fa54fe0acac2f1d0ada0d
aplactuan <aplactuan@gmail.com>
2023-05-11 19:03:10 +0800

    Show the user name that is responsible for the activity

--- resources/views/projects/activities/updated.blade.php
+++ resources/views/projects/activities/updated.blade.php
@@ -1,5 +1,5 @@
 @if (count($activity->changes['after']) == 1)
-    You updated the {{ array_key_first($activity->changes['after']) }} of the project
+    {{  $activity->user->name }} updated the {{ array_key_first($activity->changes['after']) }} of the project
 @else
-    Project was updated
+    {{  $activity->user->name }} updated the project
 @endif
